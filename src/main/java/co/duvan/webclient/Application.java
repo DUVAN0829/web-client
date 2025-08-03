1c28858fac52bda50bbff12ecc4a5c1eccbed45c
DUVAN0829 <duvancasgonz29@gmail.com>
2025-08-03 12:01:17 -0500

    Registrar componente WebClient

--- src/main/java/co/duvan/webclient/Application.java
+++ src/main/java/co/duvan/webclient/Application.java
@@ -1,13 +1,25 @@
 package co.duvan.webclient;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class Application {
 
+    //* Vasrs
+    @Value("${config.base.endpoint}")
+    private String url;
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public WebClient registrarWebClient() {
+        return WebClient.create(url);
     }
 
 }
